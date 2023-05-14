3071533723fc442b147f664f47f9e2dd1c638c85
Karl Minton <karl@iweb.co.uk>
2023-05-14 22:14:33 +0100

    added published model check, added tests to post model, added gitignore and removed unwanted cached files

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Post(models.Model):
+    __current_time = timezone.now()
+    content = models.TextField()
+    enabled = models.BooleanField(default=False)
+    published_date = models.DateTimeField()
+    unpublished_date = models.DateTimeField(default=None, null=True)
+
+    def is_published(self):
+        now = timezone.now()
+        return self.enabled == True and self.published_date < now < self.unpublished_date
+
--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Post
 
-# Create your tests here.
+
+class PostModelTests(TestCase):
+    def test_is_published_when_disabled(self):
+        start_time = timezone.now() + timezone.timedelta(days=-1)
+        end_time = timezone.now() + timezone.timedelta(days=1)
+        subject = Post(
+            content='',
+            enabled=False,
+            published_date=start_time,
+            unpublished_date=end_time
+        )
+        self.assertIs(subject.is_published(), False)
+
+    def test_is_published_within_correct_dates(self):
+        start_time = timezone.now() + timezone.timedelta(days=-1)
+        end_time = timezone.now() + timezone.timedelta(days=1)
+        subject = Post(
+            content='',
+            enabled=True,
+            published_date=start_time,
+            unpublished_date=end_time
+        )
+        self.assertIs(subject.is_published(), True)
+
+    def test_is_unpublished_after_unpublished_date(self):
+        start_time = timezone.now() + timezone.timedelta(days=-2)
+        end_time = timezone.now() + timezone.timedelta(days=-1)
+        subject = Post(
+            content='',
+            enabled=True,
+            published_date=start_time,
+            unpublished_date=end_time
+        )
+        self.assertIs(subject.is_published(), False)
+
